002ab32cb8729c1923605cdc403a3ce86a6e7294
whizmadhav11 <madhav@whizti.com>
2025-01-11 17:30:36 +0530

    adding developer name

--- app/src/main/java/com/adsprovider/jetpackcomposeonlineshopingproject1/Data.kt
+++ app/src/main/java/com/adsprovider/jetpackcomposeonlineshopingproject1/Data.kt
@@ -1,7 +1,7 @@
 package com.adsprovider.jetpackcomposeonlineshopingproject1
 
 import androidx.annotation.DrawableRes
-//Madhav Aneraye
+//Madhav Aneraye 1
 object Data {
     class Demo(
         @DrawableRes var img:Int,
